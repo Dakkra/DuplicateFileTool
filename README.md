31e9bc97e80910aaae6ea116920ba5d3599b341d
dakkra <contact@dakkra.com>
2020-01-21 17:43:34 -0700

    Not good code, but it works now
    Uses SHA256 for comparison. Works on a handful of test cases.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Tool for finding and optionally deleting duplicate files in a directory
 
 # Build
-CMake file generated and maintained by and with JetBrains CLion 2019.3. This project uses CMake 3.15 and operates under CPP14.
+CMake file generated and maintained by and with JetBrains CLion 2019.3. This project uses CMake 3.15 and operates under CPP17. CPP17 is used for the ability to use the \<filesystem\> standard header.
 
 # Libraries
 This project makes use of https://github.com/stbrumme/hash-library which is under the zlib license. See [hash-library LICENSE](hash-library/LICENSE)
--- main.cpp
+++ main.cpp
@@ -11,14 +11,75 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <fstream>
+#include <string>
+#include <filesystem>
 #include "hash-library/sha256.h"
 
-int main() {
-    std::cout << "Sha256 demo" << std::endl;
+std::string hash_of_file(const std::filesystem::path &filepath) {
+    uint32_t buff_size = 4096;
     SHA256 sha256;
-    std::string hello = "Hello, World!";
-    std::cout << sha256("Hello, World!") << std::endl;
-    std::cout << sha256(hello) << std::endl;
-    std::cout << "If strings are equal, this was a success" << std::endl;
+    //We don't want the "./" prefix
+    std::string str_path = filepath.string().substr(2, filepath.string().size());
+    //Open the file stream
+    std::ifstream input_stream(str_path);
+
+    std::string linebuf;
+    while (input_stream.good()) {
+        std::getline(input_stream, linebuf);
+        sha256.add(linebuf.c_str(), linebuf.size());
+    }
+    return sha256.getHash();
+}
+
+int main() {
+    std::cout << "Listing files in this directory with duplicates..." << std::endl;
+
+    //Setup simple data structures
+    std::unordered_map<std::string, std::vector<std::filesystem::path>> hashes_map;
+    std::unordered_map<uintmax_t, std::vector<std::filesystem::path>> sizes_map;
+    std::filesystem::path local_dir = "./";
+
+    //Find all files in this directory and map them by their size
+    for (const auto &file_path : std::filesystem::directory_iterator(local_dir)) {
+        uintmax_t size = std::filesystem::file_size(file_path);
+        sizes_map[size].push_back(file_path);
+    }
+
+    std::cout << "Files listed..." << std::endl;
+
+    //File files with similar sizes
+    std::vector<std::vector<std::filesystem::path>> duplicate_sets;
+    for (const auto &set : sizes_map) {
+        if (set.second.size() > 1) duplicate_sets.push_back(set.second);
+    }
+
+    std::cout << "Sizes analyzed..." << std::endl;
+
+    //For each set, hash and find duplicates
+    for (const auto &set : duplicate_sets) {
+        for (const auto path : set) {
+            std::string hash = hash_of_file(path);
+            hashes_map[hash].push_back(path);
+
+        }
+    }
+
+    std::cout << "Hashes generated..." << std::endl;
+
+    //Print out matches
+    std::cout << "Match groups: " << std::endl;
+    for (const auto &set_pair : hashes_map) {
+        if (set_pair.second.size() > 1) {
+            std::cout << "|- Group|" << std::endl;
+            for (const auto &path : set_pair.second) {
+                std::cout << "    -|" << path << std::endl;
+            }
+            std::cout << "|- - - -|" << std::endl;
+        }
+    }
+
     return 0;
 }
